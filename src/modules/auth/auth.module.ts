ab2101b94b543f234823c004352e972e9e8c46dd
Marcelo Meirelles <meirellesm@hotmail.com>
2023-09-16 17:33:08 -0300

    fix:remove modulo passport

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
-import { PassportModule } from "@nestjs/passport"
 import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports:[
     UsersModule, 
-    PassportModule,
     JwtModule.register({
       global:true,
       secret:process.env.SECRET_KEY,
