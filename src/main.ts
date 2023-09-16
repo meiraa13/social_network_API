86121f368e54f6902fd203c422f6f8c895e80c63
Marcelo Meirelles <meirellesm@hotmail.com>
2023-09-16 16:43:37 -0300

    feat: configs para deploy

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  app.enableCors()
+  
   const config = new DocumentBuilder()
     .setTitle('rede_social')
     .setDescription('api de gerenciamento para uma rede social')
